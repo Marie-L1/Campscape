94d424b77bb8808f805355b8cb96e5ce325da73a
Maria Lukowich <mlukowich27@gmail.com>
2024-10-21 15:34:53 -0600

    adding all the images

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,14 @@
+"use client";
+import Link from "next/link";
+
+const NavBar = () => {
+  return (
+    <nav className="nav">
+        <Link href="/">
+            <img src="" className=""></img>
+        </Link>
+    </nav>
+  )
+}
+
+export default NavBar
