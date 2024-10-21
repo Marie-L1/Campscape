8fcdbe2726d93be9abd343aaaf2c0b3a672fbd01
Maria Lukowich <mlukowich27@gmail.com>
2024-10-21 15:47:43 -0600

    adding the nav links

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,12 +1,20 @@
 "use client";
 import Link from "next/link";
+import Image from "next/image";
+import { NAV_LINKS } from "../constants";
+import { link } from "fs";
 
 const NavBar = () => {
   return (
     <nav className="nav">
         <Link href="/">
-            <img src="" className=""></img>
+            <Image src="/hilink-logo.svg" alt="Hilink Logo" width={74} height={29}/>
         </Link>
+            <ul className="nav__list">
+              {NAV_LINKS.map((link) => (
+                <Link href={link.href} key={link.key} className="nav__links">{link.label}</Link>
+              ))}
+            </ul>
     </nav>
   )
 }
