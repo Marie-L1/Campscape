5b1eaa19f16e118327a66af2c401153a0baa0acc
Maria Lukowich <mlukowich27@gmail.com>
2024-10-21 16:08:42 -0600

    setting up homepage components

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { NAV_LINKS } from "../constants";
 import { link } from "fs";
+import Button from "./Button";
 
 const NavBar = () => {
   return (
@@ -10,11 +11,26 @@ const NavBar = () => {
         <Link href="/">
             <Image src="/hilink-logo.svg" alt="Hilink Logo" width={74} height={29}/>
         </Link>
-            <ul className="nav__list">
-              {NAV_LINKS.map((link) => (
-                <Link href={link.href} key={link.key} className="nav__links">{link.label}</Link>
-              ))}
-            </ul>
+          <ul className="nav__list">
+            {NAV_LINKS.map((link) => (
+              <Link href={link.href} key={link.key} className="nav__links">{link.label}</Link>
+            ))}
+          </ul>
+          <div className="nav__login-btn">
+            <Button 
+              type="button"
+              title="Login"
+              icon="./user.svg"
+              variant="btn_dark_green"
+            />
+          </div>
+
+          <Image 
+          src="/menu.svg" 
+          alt="menu" 
+          width={32} 
+          height={32}
+          className="nav__menu-img"/>
     </nav>
   )
 }
