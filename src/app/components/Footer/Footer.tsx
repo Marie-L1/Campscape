de8c095e8432a0fe6da5c9b49d338089f1cc138c
Maria Lukowich <mlukowich27@gmail.com>
2024-11-04 16:35:50 -0700

    adding background images to the footer

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -41,6 +41,10 @@ const Footer = () => {
           </div>
         </div>
       </div>
+        <div className="footer-imgs">
+            <Image src="/images/tress.svg" alt="trees" className="footer-imgs__trees" width={50} height={50} />
+            <Image src="/images/leaf.svg" alt="leaf" className="footer-imgs__leaf" width={50} height={50} />
+        </div>
     </footer>
   )
 }
